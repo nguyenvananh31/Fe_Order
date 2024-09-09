78e019d23e3a5d09a4964e92fd7d9d4e76a575ed
Nguyễn An <annguyen04@hotmail.com>
2024-09-09 07:51:53 +0700

    an

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,11 @@ import BaseLayoutUsers from "../layout/users/BaseLayoutUsers";
 import BaseLayoutAdmin from "../layout/admin/BaseLayoutAdmin";
 
 // import DashboardPayments from "../dashboard/payments/DashboardPayments";
-import UpdatePayments from "../dashboard/payments/UpdatePayments";
-import CreatePayments from "../dashboard/payments/CreatePayments";
-
-import UpdateVoucher from "../dashboard/vouchers/UploadVouchers";
-import CreateVouchers from "../dashboard/vouchers/CreateVoucher";
-import DashboardVoucher from "../dashboard/vouchers/DashboardVoucher";
-import Dashboard from "../dashboard/chartjs/Dashboard";
 import { RoutePath } from "../constants/path";
 import Login from "../pages/admin/Login/Login";
 import Register from "../pages/admin/Register/Register";
 import ListPayment from "../pages/Payments/ListPayment";
+import ListProduct from "../pages/admin/Products/ListProduct";
 
 
 const Router = () => {
@@ -21,25 +15,26 @@ const Router = () => {
     <>
 
       <Routes >
-        <Route path={`${RoutePath.LOGIN}`}  element={<Login/>} />
-        <Route path={`${RoutePath.REGISTER}`}  element={<Register />} />
+        <Route path={`${RoutePath.LOGIN}`} element={<Login />} />
+        <Route path={`${RoutePath.REGISTER}`} element={<Register />} />
       </Routes>
       <Routes>
 
         <Route path="/" element={<BaseLayoutUsers />}>
         </Route>
         <Route path="admin" element={<BaseLayoutAdmin />}>
-        <Route index element={<Dashboard />} />
+          {/* <Route index element={<Dashboard />} /> */}
           <Route path="payments" element={<ListPayment />} />
-          <Route path="update-payments/:id" element={<UpdatePayments />} />
+          <Route path="products" element={<ListProduct />} />
+          {/* <Route path="update-payments/:id" element={<UpdatePayments />} />
           <Route path="create-payments" element={<CreatePayments />} />
-          
-          <Route path="vouchers" element={<DashboardVoucher />} />
-          <Route path="update-vouchers/:id" element={<UpdateVoucher />} />
-          <Route path="create-vouchers" element={<CreateVouchers />} />
+           */}
+          {/* <Route path="vouchers" element={<DashboardVoucher />} /> */}
+          {/* <Route path="update-vouchers/:id" element={<UpdateVoucher />} />
+          <Route path="create-vouchers" element={<CreateVouchers />} /> */}
 
         </Route>
-   
+
       </Routes>
     </>
   );
